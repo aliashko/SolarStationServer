17827cbb3a85107924edbd858e99766f4414fb3f
Alexey <alexey.liashko@gmail.com>
2022-03-20 15:34:10 +0200

    migrating .net core 6, mysql provider

--- DataAccess/SolarStationDbOptions.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SolarStationServer.DataAccess
-{
-    public class SolarStationDbOptions
-    {
-        public const string ConfigurationDefaultKey = "dataAccess";
-
-        public string ConnectionString { get; set; }
-    }
-}
---
+++ Migrations/V2__DefaultSettings.sql
@@ -0,0 +1,24 @@
+﻿CREATE TABLE Reports (
+    Id BIGINT AUTO_INCREMENT PRIMARY_KEY,
+    Date DATETIME NOT NULL,
+    Timestamp BIGINT NOT NULL,
+    Temperature FLOAT,
+    Humidity FLOAT,
+    RaindropLevel INT,
+    SoilMoistureLevel INT,
+    GsmSignalLevel INT,
+    SolarVoltage FLOAT,
+    SolarCurrent FLOAT,
+    BatteryVoltage FLOAT,
+    ArduinoVoltage FLOAT,
+    GsmVoltage FLOAT,
+    PowerMode INT,
+    SimMoneyBalance INT,
+    RestartsCount BIGINT,
+    GsmErrors INT
+);
+
+CREATE TABLE Settings (
+    Key VARCHAR(100) PRIMARY_KEY,
+    Value VARCHAR(100)
+);
